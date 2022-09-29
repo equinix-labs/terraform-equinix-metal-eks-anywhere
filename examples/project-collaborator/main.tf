5d8e2c58e8c04a61ce268272aa9ba52ed35c720d
Marques Johansson <mjohansson@equinix.com>
2022-09-29 11:38:50 -0400

    update Equinix provider to 1.10
    Signed-off-by: Marques Johansson <mjohansson@equinix.com>

--- examples/project-collaborator/main.tf
+++ examples/project-collaborator/main.tf
@@ -4,6 +4,7 @@ terraform {
   required_providers {
     equinix = {
       source = "equinix/equinix"
+      version = ">= 1.10.0"
     }
   }
 }
--- versions.tf
+++ versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     equinix = {
       source  = "equinix/equinix"
-      version = "1.8.1"
+      version = ">= 1.8.1"
     }
   }
 }
