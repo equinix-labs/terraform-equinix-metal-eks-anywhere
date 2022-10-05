c4020f0d2858fabc3e47bd6cc57a28adae8912d7
Marques Johansson <mjohansson@equinix.com>
2022-10-05 19:47:17 -0400

    labs: add optional ./replace.sh resource filtering
    Signed-off-by: Marques Johansson <mjohansson@equinix.com>

--- examples/lab/replace.sh
+++ examples/lab/replace.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
-if [ "$1" == "" ]; then echo "Usage: $0 \"email\" [*taint|untaint|rm]"; exit 1; fi
+if [ "$1" == "" ]; then echo "Usage: $0 \"email\" [*taint|untaint|rm] [admin|cp|dp|addon]"; exit 1; fi
 module="${1}"
 command=${2:-taint}
+resources="${3}"
 
 if [ "$command" == "rm" ]; then
 	echo "NOTE: Make sure '$module' is removed from the CSV file!"
@@ -9,6 +10,6 @@ if [ "$command" == "rm" ]; then
 fi
 
 # terraform state list | sed -E -e 's/\[/["/g' -e 's/\]/"]/g' -e 's/\["([0-9]+)"\]/[\1]/g' | 
-terraform state list | grep "${module}" | while read resource; do terraform $command "${resource}"; done
+terraform state list | grep "${module}" | grep -E -e "${resources}" | while read resource; do terraform $command "${resource}"; done
 
 echo Done! Run \"terraform apply\" when you are done marking instances to replace or remove.
